9af1c38a91320398a8ef79e0fcdda09082e19de0
student <student@ip-172-31-59-139.us-west-2.compute.internal>
2021-06-01 16:52:56 -0700

    Popcorn

--- src/_02_cat/catRunner.java
+++ src/_02_cat/catRunner.java
@@ -2,6 +2,11 @@ package _02_cat;
 
 public class catRunner {
 public static void main(String[] args) {
-	
+	Cat Bob = new Cat("Bob");
+Bob.meow();
+Bob.printName();
+for (int i = 0; i < 9; i++) {
+Bob.kill();
+}
 }
 }
---
+++ src/_04_popcorn/PopcornMaker.java
@@ -0,0 +1,7 @@
+package _04_popcorn;
+
+public class PopcornMaker {
+public static void main(String[] args) {
+	
+}
+}
