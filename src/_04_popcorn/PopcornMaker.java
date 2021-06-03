9b3a8ab945c5db31cce84354e9eb54d9328fc9f3
student <student@ip-172-31-48-172.us-west-2.compute.internal>
2021-06-03 16:54:35 -0700

    Popcorn

--- src/_04_popcorn/PopcornMaker.java
+++ src/_04_popcorn/PopcornMaker.java
@@ -1,7 +1,12 @@
 package _04_popcorn;
 
+import javax.swing.JOptionPane;
+
 public class PopcornMaker {
 public static void main(String[] args) {
-	
+	Popcorn Bob = new Popcorn("Bob");
+	Bob.applyHeat();
+	String Flavor = JOptionPane.showInputDialog("What flavor popcorn do you want?");
+	Bob.eat();
 }
 }
