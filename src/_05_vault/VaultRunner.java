a24bb1342b2515f9aa1bc3a9745e458c8e2752e5
student <student@ip-172-31-58-15.us-west-2.compute.internal>
2021-06-10 16:57:33 -0700

    Vault

--- src/_05_vault/VaultRunner.java
+++ src/_05_vault/VaultRunner.java
@@ -2,6 +2,9 @@ package _05_vault;
 
 public class VaultRunner {
 public static void main(String[] args) {
-	
+	Spy James = new Spy();
+	Vault TheVault=new Vault();
+int jeff = James.findCode(TheVault);
+System.out.println(jeff);
 }
 }
